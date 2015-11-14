41e9351413ff77fffb40764fcb18562ba85b6a5a
Dani Gámez Franco <gmzcodes@gmail.com>
2015-11-14 21:41:52 +0100

    Adds some tests for level 1 that will probably be removed and completed later for upcoming levels

--- Level 1 - Getting Started with Routes/js/controllers/notes-edit-controller.js
+++ Level 1 - Getting Started with Routes/js/controllers/notes-edit-controller.js
@@ -1,8 +1,18 @@
 /*global console, angular */
 
-angular.module("NoteWrangler").controller("NotesEditController", function () {
+angular.module("NoteWrangler").controller("NotesEditController", function (/*Note,*/ $http, $routeParams, $scope) {
 
     "use strict";
     
+    // $scope.note = Note.get({id: $routeParams.id});
     
+	// http://stackoverflow.com/questions/28583651/contenteditable-with-ng-model-doesnt-work
+	
+    $http({method: "GET", url: "notes.json"}).success(function (data) {
+        console.log("GET - notes.json[" + $routeParams.id + "]: ");
+        console.table(data);
+        $scope.note = data[$routeParams.id];
+    });
 });
+
+console.log("notes-edit-controller.js loaded!");
